2b873530d9b68ad90542e1a65333249a49c1884f
dangjundl <dangjundl@gmail.com>
2021-06-29 16:11:06 +0900

    Add TTS

--- app/src/main/java/com/peng/stt_test/MainActivity.java
+++ app/src/main/java/com/peng/stt_test/MainActivity.java
@@ -10,17 +10,18 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
     final int PERMISSION = 1;
     Intent intent;
     TextView textView;
     Button sttBtn;
+    Button ttsBtn;
     SpeechRecognizer mRecognizer;
 
     @Override
@@ -29,25 +30,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
   //구글 STT 예제
 
-        // 퍼미션 체크
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            ActivityCompat.requestPermissions(this, new String[]{
-//                    Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO}, PERMISSION);
+//     ActivityCompat.requestPermissions(this, new String[]{
+//               Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO}, PERMISSION);
 //        }
         // xml의 버튼과 텍스트 뷰 연결
-
         textView = (TextView) findViewById(R.id.sttResult);
         sttBtn = (Button) findViewById(R.id.sttStart);
+        ttsBtn = (Button) findViewById(R.id.tts_Intent);
         //RecognizerIntent 객체 생성
+
+
+        ttsBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intents = new Intent(getApplicationContext(), ttsActivity.class);
+                startActivity(intents);
+            }
+        });
 
         //================구글 음성인식 =============================
         intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
         mRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+
 
 
         // 버튼을 클릭 이벤트 - 객체에 Context와 listener를 할당한 후 실행
@@ -71,10 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         mRecognizer.setRecognitionListener(new RecognitionListener(){
-            @Override
-            public void onRmsChanged(float rmsdB)
-            {
-            }
+
             @Override
             public void onResults(Bundle results) {
 //                String key= "";
@@ -95,7 +102,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onReadyForSpeech(Bundle params)
             {
+                Log.d("시작??","대나??");
+
             }
+            @Override
+            public void onRmsChanged(float rmsdB)
+            {
+                Log.d("여기만타네???","대나??");
+
+            }
+
+
             @Override
             public void onPartialResults(Bundle partialResults)
             {
@@ -107,11 +124,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(int error)
             {
+                Log.d("에러?","에러임?");
+
             }
+
+            //사용자가 말을 멈춘 후에 호출됩니다.
             @Override
             public void onEndOfSpeech()
             {
+                Log.d("되나?","이거타는거임?");
+
             }
+
             @Override
             public void onBufferReceived(byte[] buffer)
             {
@@ -125,85 +149,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    private RecognitionListener listener = new RecognitionListener() {
-//        @Override
-//        public void onReadyForSpeech(Bundle params) {
-//
-//            Toast.makeText(getApplicationContext(), "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
-//        }
-//
-//        @Override
-//        public void onBeginningOfSpeech(){
-//            Log.v("DEBUG","말해주세요.");
-//
-//        }
-//
-//
-//        @Override
-//        public void onRmsChanged(float rmsdB) {
-//            Log.d("??","여깉??");
-//
-//        }
-//
-//        public void onResults (Bundle results){
-
-//            }
-//        }
-//
-//        @Override
-//        public void onBufferReceived(byte[] buffer) {}
-//
-//        @Override
-//        public void onEndOfSpeech() {}
-//
-//
-//        @Override
-//        public void onError(int error) {
-//            String message;
-//
-//            switch (error) {
-//                case SpeechRecognizer.ERROR_AUDIO:
-//                    message = "오디오 에러";
-//                    break;
-//                case SpeechRecognizer.ERROR_CLIENT:
-//                    message = "클라이언트 에러";
-//                    break;
-//                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-//                    message = "퍼미션 없음";
-//                    break;
-//                case SpeechRecognizer.ERROR_NETWORK:
-//                    message = "네트워크 에러";
-//                    break;
-//                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-//                    message = "네트웍 타임아웃";
-//                    break;
-//                case SpeechRecognizer.ERROR_NO_MATCH:
-//                    message = "찾을 수 없음";
-//                    break;
-//                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-//                    message = "RECOGNIZER가 바쁨";
-//                    break;
-//                case SpeechRecognizer.ERROR_SERVER:
-//                    message = "서버가 이상함";
-//                    break;
-//                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-//                    message = "말하는 시간초과";
-//                    break;
-//                default:
-//                    message = "알 수 없는 오류임";
-//                    break;
-//            }
-//            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
-//
-//        }
-//
-//        @Override public void onPartialResults (Bundle partialResults){}
-//
-//        @Override public void onEvent (int eventType, Bundle params){}
-//
-//
-//
-//        };
 
 }
 
